4077ede4fd1cd53a6010b20b849a209f89467b19
kaustavisi <kaustav.ndyy@gmail.com>
2015-02-03 00:56:54 +0530

    modifying geo trans functions

--- src/geoTrans.cpp
+++ src/geoTrans.cpp
@@ -3,15 +3,19 @@
 using namespace Rcpp;
 using namespace cv;
 /* Resizing generic geometrical transformation to an image */
-
+/****** INTER_NEAREST == 0 ******/
+/****** INTER_LINEAR == 1 ******/
+/****** INTER_CUBIC == 2 ******/
+/****** INTER_AREA == 3 ******/
+/****** INTER_LANCZOS4 == 4  ******/
+/*******************************************************/
 Rcpp::List imresize(Rcpp::List imgList, Rcpp::NumericVector dsize_, 
 		    double fx, double fy, int interpolation)
 {
     cv::Mat M = convertList_RCPP2CV(imgList);
     std::vector<int> dsize = as< std::vector<int> >(dsize_);
     cv::Mat outImg;
-    printf("%d,%d,%d,%d,%d",INTER_NEAREST,INTER_LINEAR,INTER_AREA,INTER_CUBIC,INTER_LANCZOS4);
-    resize(M, outImg, Size(dsize.at(0), dsize.at(1)), fx, fy, INTER_NEAREST);// interpolation);
+    resize(M, outImg, Size(dsize.at(0), dsize.at(1)), fx, fy, interpolation);
     return(convertMat_CV2RCPP(outImg));
 }
 
