7515f5cb6b56d6fb454c4407a33eda8c37a01152
kaustavisi <kaustav.ndyy@gmail.com>
2015-02-15 18:49:03 +0530

    adding geo trans functions

--- R/geoTrans.R
+++ R/geoTrans.R
@@ -21,3 +21,79 @@ imResize <- function(img, dsize, fx, fy, interp)
     mgeo <- Module("geoTrans", PACKAGE = "RIPOpenCV")
     mgeo$imresize(img, dsize, fx, fy, interp)
 }
+
+##' Applies affine warp to an image
+##'
+##' .. content for \details{} ..
+##' @title Applies affine warp to an image
+##' @param img rip object with one or three channels
+##' @param tmat 2x3 transformation matrix (rip object)
+##' @param size dimension of the output image
+##' @param flags Interpolation method 
+##' @return rip object after affine warpping
+##' @author Kaustav Nandy
+wrap.affine <- function(img, tmat, size, flags)
+{
+    mgeo <- Module("geoTrans", PACKAGE = "RIPOpenCV")
+    mgeo$affineWarp(img, tmat, size, flags)
+}
+
+##' Invert an affine transformation
+##'
+##' .. content for \details{} ..
+##' @title Invert an affine transformation
+##' @param tmat affine transformation matrix (rip object)
+##' @return Inverse affine transformation matrix as a rip object 
+##' @author Kaustav Nandy
+invertAffineTrans <- function(tmat)
+{
+    mgeo <- Module("geoTrans", PACKAGE = "RIPOpenCV")
+    mgeo$affineTransInvert(tmat)
+}
+
+##' Apples perspective transformation to an image
+##'
+##' .. content for \details{} ..
+##' @title Perspective Transformation
+##' @param img rip object of one or three channels
+##' @param tmat 3x3 transformation matrix
+##' @param size dimension of the output rip object
+##' @return transformed rip object of same type as img
+##' @author Kaustav Nandy
+warp.perspective <- function(img, tmat, size)
+{
+    mgeo <- Module("geoTrans", PACKAGE = "RIPOpenCV")
+    mgeo$perspectiveWarp(img, tmat, size)
+}
+
+##' Retrieves a pixel rectangle from an image with sub-pixel accuracy.
+##'
+##' .. content for \details{} ..
+##' @title Retrives pixel rectangle
+##' @param img rip object
+##' @param patchSize dimension of the output retcangle rip object patch
+##' @param center coordinates of the center of the extracted rectangle within the source image
+##' @param patchType depth of the extracted pixels. Negative value means the same depth as \code{img}
+##' @return 
+##' @author Kaustav Nandy
+getRectSubPix <- function(img, patchSize, center, patchType=-1)
+{
+    mgeo <- Module("geoTrans", PACKAGE = "RIPOpenCV")
+    mgeo$rectSubPix(img, patchSize, center, patchType)
+}
+
+
+##' Calculates an affine matrix of 2D rotation
+##'
+##' .. content for \details{} ..
+##' @title Affine matrix of 2D rotation
+##' @param center center of the rotation in source image
+##' @param angle Rotation angle in degrees. Positive values mean counter-clockwise rotation (the coordinate origin is assumed to be the top-left corner)
+##' @param scale Isotropic scale factor
+##' @return 2x3 rip object of affine transformation
+##' @author Kaustav Nandy
+getRotationMat <- function(center, angle, scale)
+{
+    mgeo <- Module("geoTrans", PACKAGE = "RIPOpenCV")
+    mgeo$getRotMat(center, angle, scale)
+}
--- src/geoTrans.cpp
+++ src/geoTrans.cpp
@@ -23,7 +23,64 @@ Rcpp::List imresize(Rcpp::List imgList, Rcpp::NumericVector dsize_,
 }
 
 
+Rcpp::List affineWarp(Rcpp::List imgList, Rcpp::List tmat_, Rcpp::NumericVector dsize_, 
+		      int flags)
+{
+    cv::Mat M = convertList_RCPP2CV(imgList);
+    cv::Mat tmat = convertList_RCPP2CV(tmat_);
+    std::vector<int> dsize = as< std::vector<int> >(dsize_);
+    cv::Mat outImg;
+    warpAffine(M, outImg, tmat, Size(dsize.at(0), dsize.at(1)));
+    return(convertMat_CV2RCPP(outImg));
+}
+
+
+Rcpp::List affineTransInvert(Rcpp::List tmat_)
+{
+    cv::Mat tmat = convertList_RCPP2CV(tmat_);
+    cv::Mat itmat;
+    invertAffineTransform(tmat, itmat);
+    return(convertMat_CV2RCPP(itmat));
+}
+
+Rcpp::List perspectiveWarp(Rcpp::List imgList, Rcpp::List tmat_, 
+			   Rcpp::NumericVector size_)
+{
+    cv::Mat M = convertList_RCPP2CV(imgList);
+    cv::Mat tmat = convertList_RCPP2CV(tmat_);
+    std::vector<int> size = as< std::vector<int> >(size_);
+    cv::Mat outImg;
+    warpPerspective(M, outImg, tmat, Size(size.at(0), size.at(1)));
+    return(convertMat_CV2RCPP(outImg));
+}
+
+
+Rcpp::List rectSubPix(Rcpp::List imgList, Rcpp::NumericVector patchsize_, 
+		      Rcpp::NumericVector center_, int patchtype)
+{
+    cv::Mat M = convertList_RCPP2CV(imgList);
+    std::vector<int> patchsize = as< std::vector<int> >(patchsize_);
+    std::vector<int> center = as< std::vector<int> >(center_);
+    cv::Mat outImg;
+    Point2f centerP(center.at(0), center.at(1));
+    getRectSubPix(M, Size(patchsize.at(0), patchsize.at(1)), centerP, outImg, patchtype);
+    return(convertMat_CV2RCPP(outImg));
+}
+
+Rcpp::List getRotMat(Rcpp::NumericVector center_, double angle, double scale)
+{
+    std::vector<int> center = as< std::vector<int> >(center_);
+    Point2f centerP(center.at(0), center.at(1));
+    cv::Mat outMat = getRotationMatrix2D(centerP, angle, scale);
+    return(convertMat_CV2RCPP(outMat));
+}
+
 RCPP_MODULE(geoTrans) 
 {
     function("imresize", &imresize, "Image resizing");
+    function("affineWarp", &affineWarp, "Affine warpping");
+    function("affineTransInvert", &affineTransInvert, "inverts an affine transformation");
+    function("perspectiveWarp", &perspectiveWarp, "Perspective Warpping");
+    function("rectSubPix", &rectSubPix, "Retrives a pixel rectangle");
+    function("getRotMat", &getRotMat, "get rotation matrix");
 }
