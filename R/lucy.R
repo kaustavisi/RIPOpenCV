979c108075fbc563590af4be257f7e92c49be7c8
kaustavisi <kaustav.ndyy@gmail.com>
2015-06-26 05:38:52 +0530

    modifying RL Algo

--- R/lucy.R
+++ R/lucy.R
@@ -7,15 +7,16 @@
 ##' @param intiImage Initial guess
 ##' @param kernel blur kernel
 ##' @param iter number of iteration
+##' @param ind Method to be use. FIXME: Detele it upon checking
 ##' @return Deblurred rip image.
 ##' @author Kaustav Nandy
 
-rlalgo <- function(origImage, initImage, kernel, iter)
+rlalgo <- function(origImage, initImage, kernel, iter, ind)
 {
     mrla <- Module("rla", PACKAGE = "RIPOpenCV")
     if(sum(dim(origImage$Data) != dim(initImage$data)) != 0)
         stop("initImage should be of same size as origImage")
     if((origImage$Dimension)[3] != 1)
         stop("Only single channel images are allowed")
-    mrla$RLA(origImage, initImage, kernel, iter)
+    mrla$RLA(origImage, initImage, kernel, iter, ind)
 }
