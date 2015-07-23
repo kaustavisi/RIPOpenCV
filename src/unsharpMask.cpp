db80956af9d59b0c164a2488daf497bb521f7fb0
kaustav nandy <kaustav@kaustav>
2015-07-23 22:59:25 +0530

    modification

--- src/unsharpMask.cpp
+++ src/unsharpMask.cpp
@@ -10,7 +10,7 @@ Rcpp::NumericMatrix unsharp(Rcpp::NumericMatrix imgMat, Rcpp::NumericVector size
     std::vector<int> size = as< std::vector<int> >(size_);
     cv::Mat outImg;
     GaussianBlur(M, outImg, Size(size.at(0), size.at(1)), sigmaX, sigmaY);
-    addWeighted(M, 1.5, outImg, -.5, 0, outImg);
+    addWeighted(M, 1.5, outImg, -0.5, 0, outImg);
     return (convertMat_CV2RCPP(outImg));
 }
 
